8c7f20db9752ee28afed23fdd5b4f0ed789e028f
Pasi Niemi <ext-pasi.1.niemi@nokia.com>
2015-01-21 11:31:41 +0200

    Switch script name to match artifactId

--- src/main/shell/create-script.sh
+++ src/main/shell/create-script.sh
@@ -3,7 +3,7 @@
 set -x
 
 MD5=$(md5sum target/javaxdelta-*-jar-with-dependencies.jar | cut -d " " -f 1)
-ARCHIVE_START=$(($(wc -l  src/main/shell/jarxdelta.sh | cut -d" " -f1) + 1))
-sed -e "s/%%MD5%%/$MD5/" -e "s/%%ARCHIVE_START%%/$ARCHIVE_START/" src/main/shell/jarxdelta.sh > target/jarxdelta.sh
-cat target/javaxdelta-*-jar-with-dependencies.jar >> target/jarxdelta.sh
-chmod 755 target/jarxdelta.sh
+ARCHIVE_START=$(($(wc -l  src/main/shell/javaxdelta.sh | cut -d" " -f1) + 1))
+sed -e "s/%%MD5%%/$MD5/" -e "s/%%ARCHIVE_START%%/$ARCHIVE_START/" src/main/shell/javaxdelta.sh > target/javaxdelta.sh
+cat target/javaxdelta-*-jar-with-dependencies.jar >> target/javaxdelta.sh
+chmod 755 target/javaxdelta.sh
